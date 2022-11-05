99ea3d364345e076cc9a1a3ae483e5d42cd1863f
Avdheshg <42843901+Avdheshg@users.noreply.github.com>
2022-11-05 20:13:11 +0530

    Added solution - LeetHub

---
+++ Count total set bits - GFG/count-total-set-bits.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    public static int countSetBits(int n){
+    
+        // Your code here
+        if(n==0){
+            return 0;
+        }
+        int x=largestPowerOf2(n);
+        
+        int a= x * (1<<(x-1));     //(2^x-1)*x
+        int b= n-(1<<x)+1;       //(n-(2^x)+1)
+        int c= n-(1<<x);            //(n-(2^x))
+        
+        int ans=a+b+countSetBits(c);
+        return ans;
+    }
+    
+    static int largestPowerOf2(int n){
+        int x=0;
+        
+        while((1<<x)<=n){
+            x++;
+        }
+        return x-1;
+    }
+}
+
+//{ Driver Code Starts.
+
+// Driver code
+class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();//testcases
+		int x, n;
+		while(t-->0) {
+	        n = sc.nextInt();//input n
+
+		    Solution obj = new Solution();
+
+		    System.out.println(obj.countSetBits(n));//calling countSetBits() method
+		}
+	}
+}
+
+// } Driver Code Ends
