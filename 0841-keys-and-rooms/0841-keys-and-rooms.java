ed512beef3c37cd2280b3945f9ae5146aaacc9e8
Avdheshg <42843901+Avdheshg@users.noreply.github.com>
2022-11-07 07:12:19 +0530

    Time: 6 ms (24.89%), Space: 45.2 MB (18.32%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -23,3 +23,27 @@ class Solution {
         return visitedRooms.size() == rooms.size();
     }
 }
+
+/* 
+        Why hashset?
+        we used as  hashset for two reasons
+	        1. track the rooms which we are already visited or opened
+	        2. at the end, after the traversal of all the rooms, this hashset will be used to decide whether we have opened all the rooms or not
+
+        why stack? 
+        we are going to use dfs algorithm which means that after opening the current room whichever keys we will get from this room we will open that room in the next iteration
+
+        Algorithm
+            * we will define hashset for visited rooms and add 0 as visited room
+            * define a stack so that we can hold the keys which we have got from the current room and add 0  to the stack which means that we have keys for 0th room as given in the question
+            * loop until we have keys left the stack
+                * pop from the stack it means that we have opened the room by taking the keys from the stack
+                * open the room from the popped key and get the keys present in this room
+                * Traverse all the keys which we have got after opening the room
+                    * check if if for the current key the room is already opened
+                        * if not opened then mark the current room as visited
+                        * add the current key to the stack so that in future we can open this room
+
+            * at the end if the visited rooms size is equal to the given room size it means that we have opened all the rooms successfully
+
+     */
